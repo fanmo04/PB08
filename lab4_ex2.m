b1ef9ec007c0849cf085699c8d492960ab6871de
fanmo04 <michaelamofan@yahoo.co.uk>
2017-02-08 09:48:53 +0000

    Add files via upload

---
+++ lab4_ex2.m
@@ -0,0 +1,27 @@
+%Lab 4 -Exercise 2: 3D display of roll and pitch angles
+clear all
+close('all')
+pb = PyBench('COM4');
+model = IMU_3D();
+N = 50;
+tic;
+gx = 0; gy = 0;
+fig1 = figure(1);
+while true
+    for i = 1:N
+        [p,r] = pb.get_accel();
+        [x,y,z] = pb.get_gyro();
+        dt = toc;
+        tic;
+        pitch = p*180/pi;
+        roll = r*180/pi;
+        gx = max(min(gx+x*dt, pi/2),-pi/2);
+        gy = max(min(gy+y*dy,pi/2),-pi/2);
+        clf(fig1);
+        subplot(2,1,1);
+        model.draw(fig1, p, r, 'Accelerometer');
+        subplot(2, 1, 2);
+        model.draw(fig1, gy, gx, 'Gyroscope');
+        pause(0.0001);
+    end %for
+end %while
