3d52980114d261f0e51d055abd4b5aa50d9e8c2c
fanmo04 <michaelamofan@yahoo.co.uk>
2017-02-08 11:31:44 +0000

    Create lab4_ex4.py

---
+++ lab4_ex4.py
@@ -0,0 +1,33 @@
+'''
+-----------------------------------------------------------
+Name: Lab 4 Exercise 4
+-----------------------------------------------------------
+Learning to use rhe OLED deisplay driver
+-----------------------------------------------------------
+'''
+import pyb
+from pyb import LED, ADC, Pin  # Pyboard basic library
+from oled_938 import OLED_938  # Use various class libraries in pyb
+
+# Create peripheral objects
+b_LED = LED(4)
+pot = ADC(Pin('X11'))
+
+# I2C connected to Y9, Y10 (I2C bus 2) and Y11 is reset low active
+oled = OLED_938(pinout = {'sda': 'Y10', 'scl': 'Y9', 'res': 'Y8'}, height = 64, external_vcc = False, i2c_devid = 61)
+oled.poweron()
+oled.init_display()
+
+# Simple Hello world message
+oled.draw_text(0, 0, 'Hello, world!')  # each character is 6x8 pixels
+
+tic = pyb.mills()  # store starting time
+while True:
+  b_LED.toggle()
+  toc = pyb.mills() ' read elapsed time
+  oled.draw_text(0, 20, 'Delayed time:{:6.3f}sec'.format((toc-tic)*0.001))
+  oled.draw_text(0, 40, 'POT5K reading:{:5d}'.format(pot.read()))
+  tic = mills() # start time
+  oled.display()
+  delay = pyb.rng()%1000 # Generate a random number btw 0 and 999
+  pyb.delay(delay)       # Delay in milliseconds
